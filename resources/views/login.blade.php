065f67b9c8b80579314059568dcfa59518af98ce
salmafebrina <salmafebrina29@gmail.com>
2026-08-12 05:06:52 +0700

    Perbaiki halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Puskesmas Somagede</title>
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/login.css') }}">
+<script src="{{ asset('public/js/login.js') }}"></script>
 </head>
 <body>
 
